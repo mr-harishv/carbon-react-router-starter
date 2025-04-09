0a93a4d50ab4d40e1374df496ef86302d5c848f4
Lee Chase <lee.chase@uk.ibm.com>
2025-04-09 09:06:11 +0100

    feat: propose eslint plugins hooks and refresh (#22)
    * feat: propose eslint plugins hooks and refresh
    
    * chore: add lint script
    
    * fix: align settings with lint:es
    
    ---------
    
    Co-authored-by: Mike <49228392+Mikadv@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
-import pluginJs from "@eslint/js";
+import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {ignores: ["dist/**/*", "node_modules", "vite.config.js"]},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { ignores: ['dist/**/*', 'node_modules', 'vite.config.js'] },
   {
     languageOptions: {
       globals: {
@@ -17,18 +18,20 @@ export default [
       },
       parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
-    }
+        sourceType: 'module',
+      },
+    },
   },
   pluginJs.configs.recommended,
   importPlugin.flatConfigs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     ...pluginReact.configs.flat['jsx-runtime'],
+    ...pluginReactHooks.configs['recommended-latest'],
+    ...pluginReactRefresh.configs.recommended,
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
@@ -37,30 +40,38 @@ export default [
       'import/order': [
         'error',
         {
-          groups: [["builtin", "external"], "internal", ["sibling", "parent"], "object"],
+          groups: [
+            ['builtin', 'external'],
+            'internal',
+            ['sibling', 'parent'],
+            'object',
+          ],
           pathGroups: [
             {
-              pattern: "**/*.+(css|scss)",
-              patternOptions: {dot: true, nocomment: true},
-              group: "unknown",
-              position: "after",
+              pattern: '**/*.+(css|scss)',
+              patternOptions: { dot: true, nocomment: true },
+              group: 'unknown',
+              position: 'after',
             },
             {
-              pattern: "{.,..}/**/*.+(css|scss)",
-              patternOptions: {dot: true, nocomment: true},
-              group: "unknown",
-              position: "after",
+              pattern: '{.,..}/**/*.+(css|scss)',
+              patternOptions: { dot: true, nocomment: true },
+              group: 'unknown',
+              position: 'after',
             },
           ],
           alphabetize: {
-            order: "asc",
-            orderImportKind: "asc",
+            order: 'asc',
+            orderImportKind: 'asc',
             caseInsensitive: true,
           },
-          "newlines-between": "always",
+          'newlines-between': 'always',
         },
       ],
-      "sort-imports": ["error", {ignoreCase: true, ignoreDeclarationSort: true}],
-    }
-  }
+      'sort-imports': [
+        'error',
+        { ignoreCase: true, ignoreDeclarationSort: true },
+      ],
+    },
+  },
 ];
